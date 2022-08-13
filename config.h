3b0d24e067511c8254491b7cc67a7c66529671bf
complex2liu <contact@complex2math.com>
2022-08-13 16:04:30 +0800

    config.h: use "size" instead of "pixelsize"

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]    = { "monospace:size=14",
-                                  "Ping Fang SC:pixelsize=14:antialias=true:autohint=true",
-                                  "Apple Color Emoji:pixelsize=14:antialias=true:autohint=true",
+                                  "Ping Fang SC:size=14:antialias=true:autohint=true",
+                                  "Apple Color Emoji:size=14:antialias=true:autohint=true",
                                 };
 static char dmenufont[]       = "monospace:size=14";
 static char normbgcolor[]     = "#222222";
