a525084c862d008e794a5242f61c89c4e66e06af
complex2liu <contact@complex2math.com>
2022-08-14 05:15:33 +0800

    Provide comprehensive utilities for managing the client stack
    It implements two new commands: `focusstack` (which is a replacement for
    the original `focusstack` command) and `pushstack`. The first one is for
    focusing clients while the second one moves clients around the stack.
    
    See https://dwm.suckless.org/patches/stacker/

--- config.h
+++ config.h
@@ -64,6 +64,10 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+#define STACKKEYS(MOD,ACTION) \
+    { MOD,  XK_j,	ACTION##stack,	{.i = INC(+1) } }, \
+    { MOD,  XK_k,	ACTION##stack,	{.i = INC(-1) } }, \
+    { MOD,  XK_o,   ACTION##stack,  {.i = 0 } },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -135,8 +139,10 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = +0.125} },
     { MODKEY|ShiftMask,             XK_0,      setcfact,       {.f =  0.00} },
 
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+    /* manage client stack */
+    STACKKEYS(MODKEY,                          focus)
+    STACKKEYS(MODKEY|ShiftMask,                push)
+
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
